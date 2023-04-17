12de56fa849e7ff9672b6b58357ef13cbacdcd5e
Harshana Wijayawardana <dewmini.here@gmail.com>
2023-04-17 20:19:10 +0200

    Get user input from user

---
+++ myscript.py
@@ -0,0 +1,14 @@
+# this is my first python program
+# version 1.0.0.0
+
+#define a variable and set my name
+name = "Dewmi"
+print ("My name is", name)
+
+
+
+# get user input from user
+newName = input("enter your name: ")
+print ("hello", newName, "!!!")
+
+
